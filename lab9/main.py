ffda8947561d945f3ced9a0d306e5b59c5b3186b
Aleksei Marov <aleksei.marov@ionos.com>
2021-05-17 23:33:55 +0200

    improved printing

--- lab9/main.py
+++ lab9/main.py
@@ -1,22 +1,12 @@
 import math
 
 import numpy as np
-
+np.set_printoptions(precision=8)
 
 def F(x, Y):
-    return np.asarray([x * math.sin(Y[1]),
+    return np.asarray([math.sin(x * Y[1]),
                        x+math.cos(x*Y[0])])
 
-def euler(F, u0, tau, T):
-    N_t = int(round(T/tau))
-    F_ = lambda t, u: np.asarray(F(t, u))
-    t = np.linspace(0, N_t*tau, N_t+1)
-    u = np.zeros((N_t+1, len(u0)))
-    u[0] = np.array(u0)
-    for n in range(N_t):
-        u[n+1] = u[n] + tau*F_(t[n], u[n])
-
-    return u, t
 
 
 def eu(f, h, a, b, y_ic):
@@ -24,10 +14,9 @@ def eu(f, h, a, b, y_ic):
     y = y_ic
 
     while (x <= b):
-        print(x, y)
+        print("x=%.8f" % x, " y=", y)
         y = y + h * f(x, y)
         x = x + h
     return y
 
-# print("euler result: ", eu(F, 0.1, 0, 3, [0 , 0]))
 print("euler result: ", eu(F, 0.1, 0, 2, [0 , 0]))
